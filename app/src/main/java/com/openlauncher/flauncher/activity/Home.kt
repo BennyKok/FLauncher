40f82ec7e08eccdc0a86201883fb42ce7c41790f
BennyKok <itechbenny@gmail.com>
2018-03-11 19:43:47 +0800

    *Now with transparent status and navigation *Now using Aesthetic to theme the launcher, however may crash in emulator  but works on real device.

--- app/src/main/java/com/openlauncher/flauncher/activity/Home.kt
+++ app/src/main/java/com/openlauncher/flauncher/activity/Home.kt
@@ -1,35 +1,57 @@
 package com.openlauncher.flauncher.activity
 
-import android.app.Activity
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.ftinc.scoop.Scoop
-import com.ftinc.scoop.ui.ScoopSettingsActivity
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
+import com.afollestad.aesthetic.Aesthetic
+import com.mikepenz.fastadapter.commons.adapters.FastItemAdapter
 import com.openlauncher.fcore.manager.app.AppManager
 import com.openlauncher.flauncher.R
+import com.openlauncher.flauncher.model.item.AppItem
+import kotlinx.android.synthetic.main.activity_home.*
 
 class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Aesthetic.attach(this)
         super.onCreate(savedInstanceState)
-        Scoop.getInstance().apply(this)
         AppManager.init(packageManager)
         setContentView(R.layout.activity_home)
 
-        startActivityForResult(ScoopSettingsActivity.createIntent(this, "Change Themes"),46)
+        if (Aesthetic.isFirstTime()) {
+            Aesthetic.get()
+                    .colorPrimaryRes(R.color.colorPrimary)
+                    .colorAccentRes(R.color.colorAccent)
+                    .apply()
+        }
+
+
+        initSimpleAppList()
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun initSimpleAppList() {
+        allAppsList.layoutManager = GridLayoutManager(this, 4)
+        val fastItemAdapter = FastItemAdapter<AppItem>()
+        allAppsList.adapter = fastItemAdapter
 
-        if (resultCode == Activity.RESULT_OK){
-            if (requestCode == 46){
-                val restart = Intent(this, Home::class.java)
-                finish()
-                startActivity(restart)
-                overridePendingTransition(0, 0)
+        AppManager.getAllApps().subscribe { allApps ->
+            fastItemAdapter.set(allApps.map { AppItem(it) })
+            fastItemAdapter.withOnClickListener { _, _, item, _ ->
+                item.app.start(this)
+                true
             }
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        Aesthetic.resume(this)
+    }
+
+    override fun onPause() {
+        Aesthetic.pause(this)
+        super.onPause()
+    }
+
+
 }
